6cd4bf28cccb478ecb43e59445d008f5ced43287
privetinternet <nikitakanashenko@gmail.com>
2019-01-30 14:50:48 +0200

    Second commit

--- src/com/task/OpenAddressingHashMap.java
+++ src/com/task/OpenAddressingHashMap.java
@@ -24,11 +24,12 @@ public class OpenAddressingHashMap {
 			return "Entry [key=" + key + ", value=" + value + "]";
 		}
 	}
-
-	private final int DEFAULT_TABLE_SIZE = 5;
+		
+	private final int DEFAULT_TABLE_SIZE = 5;	
 	private int threshold = (int) (0.75f * DEFAULT_TABLE_SIZE);
 	private int num_of_elements = 0;
-	public Entry[] table;
+	private Entry[] table;
+	
 
 	OpenAddressingHashMap() {
 		table = new Entry[DEFAULT_TABLE_SIZE];
@@ -36,25 +37,7 @@ public class OpenAddressingHashMap {
 			table[i] = null;
 		}
 	}
-
-	private int hash(int key, int probeNumber) {
-
-		return (key + probeNumber) % table.length;
-	}
-
-	private void resize() {
-		int newSize = 2 * table.length;
-		threshold = (int) (0.75f * newSize);
-		Entry[] oldTable = table;
-		table = new Entry[newSize];
-		num_of_elements = 0;
-		for (int i = 0; i < oldTable.length; i++) {
-			if (oldTable[i] != null) {
-				put(oldTable[i].getKey(), oldTable[i].getValue());
-			}
-		}
-	}
-
+	
 	public void put(int key, long value) {
 		int probeNumber = 0;
 		int index;
@@ -98,4 +81,22 @@ public class OpenAddressingHashMap {
 	public int size() {
 		return num_of_elements;
 	}	
+	
+	private int hash(int key, int probeNumber) {
+
+		return (key + probeNumber) % table.length;
+	}
+
+	private void resize() {
+		int newSize = 2 * table.length;
+		threshold = (int) (0.75f * newSize);
+		Entry[] oldTable = table;
+		table = new Entry[newSize];
+		num_of_elements = 0;
+		for (int i = 0; i < oldTable.length; i++) {
+			if (oldTable[i] != null) {
+				put(oldTable[i].getKey(), oldTable[i].getValue());
+			}
+		}
+	}
 }
