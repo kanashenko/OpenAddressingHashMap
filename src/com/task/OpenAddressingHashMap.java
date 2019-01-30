394c7219adb6cfb0370b90fb7f17a6c689d977ce
privetinternet <nikitakanashenko@gmail.com>
2019-01-30 14:39:46 +0200

    first commit

---
+++ src/com/task/OpenAddressingHashMap.java
@@ -0,0 +1,101 @@
+package com.task;
+
+public class OpenAddressingHashMap {
+
+	private class Entry {
+		int key;
+		long value;
+
+		public Entry(int key, long value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public int getKey() {
+			return key;
+		}
+
+		public long getValue() {
+			return value;
+		}
+
+		@Override
+		public String toString() {
+			return "Entry [key=" + key + ", value=" + value + "]";
+		}
+	}
+
+	private final int DEFAULT_TABLE_SIZE = 5;
+	private int threshold = (int) (0.75f * DEFAULT_TABLE_SIZE);
+	private int num_of_elements = 0;
+	public Entry[] table;
+
+	OpenAddressingHashMap() {
+		table = new Entry[DEFAULT_TABLE_SIZE];
+		for (int i = 0; i < DEFAULT_TABLE_SIZE; i++) {
+			table[i] = null;
+		}
+	}
+
+	private int hash(int key, int probeNumber) {
+
+		return (key + probeNumber) % table.length;
+	}
+
+	private void resize() {
+		int newSize = 2 * table.length;
+		threshold = (int) (0.75f * newSize);
+		Entry[] oldTable = table;
+		table = new Entry[newSize];
+		num_of_elements = 0;
+		for (int i = 0; i < oldTable.length; i++) {
+			if (oldTable[i] != null) {
+				put(oldTable[i].getKey(), oldTable[i].getValue());
+			}
+		}
+	}
+
+	public void put(int key, long value) {
+		int probeNumber = 0;
+		int index;
+		do {
+			index = hash(key, probeNumber);
+			if (table[index] != null) {
+				if (table[index].key == key) {
+					table[index].value = value;
+					num_of_elements--;
+					break;
+				}
+			}
+			probeNumber++;
+		} while (table[index] != null);
+
+		table[index] = new Entry(key, value);
+		num_of_elements++;
+		if (num_of_elements >= threshold) {
+			resize();
+		}
+	}
+
+	public Long get(int key) {
+		int probeNumber = 0;
+		int index;
+		Long value = null;
+		for (int i = 0; i < table.length; i++) {
+			index = hash(key, probeNumber);
+			if (table[index] == null) {
+
+			} else if (table[index].key != key) {
+
+			} else {
+				value = table[index].value;
+			}
+			probeNumber++;
+		}
+		return value;
+	}
+
+	public int size() {
+		return num_of_elements;
+	}	
+}
---
+++ src/com/task/OpenAddressingHashMapTest.java
@@ -0,0 +1,65 @@
+package com.task;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import org.junit.jupiter.api.Test;
+
+class OpenAddressingHashMapTest {
+	
+	@Test
+	void test() {		
+		OpenAddressingHashMap map = new OpenAddressingHashMap();	
+		
+		assertEquals(0, map.size());
+		
+		map.put(77, 7000);		
+		map.put(44, 12000);
+		map.put(35, 1000);
+		map.put(50, 5000);
+		
+		assertEquals(map.size(), 4); 
+
+		assertEquals(map.get(77), new Long(7000));		
+		assertEquals(map.get(44), new Long(12000));
+		assertEquals(map.get(35), new Long(1000));
+		assertEquals(map.get(50), new Long(5000));
+				
+		map.put(50, 144);
+		assertEquals(map.get(50), new Long(144));
+		
+		assertNull(map.get(99));
+	}
+	
+	@Test
+	void test2() {
+		OpenAddressingHashMap map1 = new OpenAddressingHashMap();
+		OpenAddressingHashMap map2 = new OpenAddressingHashMap();
+		map1.put(77, 7000);		
+		map2.put(77, 7000);	
+		assertEquals(map1.size(), map2.size());
+		assertEquals(map1.get(77), map2.get(77));					
+	}
+	
+	@Test
+	void testCollisions() {
+		OpenAddressingHashMap map = new OpenAddressingHashMap();
+		
+		map.put(5, 1);		
+		map.put(10, 2);
+		map.put(15, 3);
+		
+		map.put(7, 4);		
+		map.put(14, 5);
+		map.put(21, 6);
+		map.put(28, 7);
+		
+		assertEquals(map.get(5), new Long(1));		
+		assertEquals(map.get(10), new Long(2));
+		assertEquals(map.get(15), new Long(3));
+		assertEquals(map.get(7), new Long(4));
+		assertEquals(map.get(14), new Long(5));
+		assertEquals(map.get(21), new Long(6));
+		assertEquals(map.get(28), new Long(7));		
+	}
+
+}
